53d10831403cc04dcf750f19648ea8f08c78ef76
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-06-02 15:31:11 +1000

    Expanded tests for error handling in page playoff system

--- spec/tournament/page_playoff_spec.rb
+++ spec/tournament/page_playoff_spec.rb
@@ -36,6 +36,19 @@ describe Tournament::PagePlayoff do
   describe '#generate' do
     let(:teams) { [1, 2, 3, 4] }
 
+    it 'handles invalid round option' do
+      driver = TestDriver.new(teams: teams)
+      expect { described_class.generate driver, round: 3 }.to raise_exception Exception
+    end
+
+    it 'handles invalid number of teams' do
+      driver = TestDriver.new(teams: [1, 2, 3])
+      expect { described_class.generate driver }.to raise_exception Exception
+
+      driver = TestDriver.new(teams: [1, 2, 3, 4, 5])
+      expect { described_class.generate driver }.to raise_exception Exception
+    end
+
     context 'semi-finals' do
       it 'works' do
         driver = TestDriver.new(teams: teams)
